823aa61b377ccc1b78c9334ee5c74fac0255d901
YeemiRouth <yeemirouth@gmail.com>
2023-11-08 02:47:45 +1100

    m29 pistol

--- Entitybase/Guns.cs
+++ Entitybase/Guns.cs
@@ -2,9 +2,15 @@
 
 class Guns
 {
-    public static class AK
+    public static class AK // assault rifle
     {
         public static Point RecoilVecZoom = new Point(-3, 7);
         public static Point RecoilVec = new Point(-2, 4);
+    }
+
+    public static class M29 // m29 pistol
+    {
+        public static Point RecoilVecZoom = new Point(-1, 10);
+        public static Point RecoilVec = new Point(-1, 12);
     }
 }
