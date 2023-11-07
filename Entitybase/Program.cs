bd5a2710e518db5da2a38a888e23a00ba3fcdb22
YeemiRouth <yeemirouth@gmail.com>
2023-11-07 15:57:18 +1100

    Add project files.

---
+++ Entitybase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+// use at your own risk
+class Program
+{
+    Stopwatch akStopwatch = new Stopwatch();
+
+    static void Main(string[] args)
+    {
+        new Keymap(); // init
+
+        Keymap.globalKeyEvent += OnKeyEvent;
+
+        while (true)
+        {
+            Thread.Sleep(1);
+            Keymap.handle.KeyTick();
+        }
+    }
+
+    private static void OnKeyEvent(object sender, KeyEvent e)
+    {
+        if (e.key == Keys.LControlKey && e.vkey == VKeyCodes.KeyDown)
+        {
+            Console.WriteLine("lctrl down");
+        }
+    }
+}
