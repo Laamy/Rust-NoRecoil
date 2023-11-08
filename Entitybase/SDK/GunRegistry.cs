c6ead41fe0c7f950c862ae6574df137e62c72725
YeemiRouth <yeemirouth@gmail.com>
2023-11-08 17:22:10 +1100

    mp5 added & fixed m92 naming

--- Entitybase/SDK/GunRegistry.cs
+++ Entitybase/SDK/GunRegistry.cs
@@ -8,7 +8,8 @@ class GunRegistry
     public static Dictionary<string, Gun> Guns = new Dictionary<string, Gun>()
     {
         { "Assault Rifle", new Gun(new Point(-2, 4), new Point(-3, 7)) },
-        { "M29 Pistol", new Gun(new Point(-1, 12), new Point(-1, 10)) },
+        { "M92 Pistol", new Gun(new Point(-1, 12), new Point(-1, 10)) },
+        { "MP5", new Gun(new Point(0, 4), new Point(0, 3)) }, // 3 for slightly up 4 for slightly down I need a better way to measure recoil then pixels
     };
 
     public static Gun Get(int index) => Guns.ElementAtOrDefault(index).Value;
